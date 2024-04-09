bd78a1f2a65383216739af77c65c91020792b0f2
fabianNav <fn39001@gmail.com>
2024-04-09 19:34:56 -0300

    Terminamos la logica de la calculadora, ahora veo si agrego un par de cosas

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 import './css/style.css'
+
+const display = document.getElementById("display");
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (btn.id === "=") {
+      display.value = eval(display.value);
+    } else if (btn.id === "resetear") {
+      display.value = "";
+    } else if (btn.id == "borrar") {
+      display.value = display.value.slice(0, -1);
+    } else {
+      display.value += btn.id;
+    }
+  })
+} )
